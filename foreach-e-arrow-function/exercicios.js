bbeeae9035eb1f676ca19d4529d218c4e3392f85
Giovani <giovanifrota45@gmail.com>
2025-08-15 09:28:31 -0300

    exercicios de forEach e Arrow Function concluidos

--- foreach-e-arrow-function/exercicios.js
+++ foreach-e-arrow-function/exercicios.js
@@ -1,24 +1,23 @@
-//Retorne no console todas as imagens do site
-
-const imagens = document.querySelectorAll('img');
-console.log(imagens);
-
-//Retorne   no console apenas as imagens que começaram com a palavra imagem
-
-const imagensImg = document.querySelectorAll('img[src^="img/imagem"]');
-console.log(imagensImg);
-
-//Selecione todas as linhas internas (onde o href começa com #)
-
-const linkInterno = document.querySelectorAll('[href^="#"]');
-console.log(linkInterno);
-
-//Selecione o primeiro h2 dentro de .animais- descrição
-
-const primeiroH2 = document.querySelector('.animais-descricao h2')
-console.log(primeiroH2.innerHTML);
-
-//Selecione o ultimo p do site
+//Mostre no console cada paragrafo do site
 
 const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos[paragrafos.length - 1])
+
+paragrafos.forEach((item)=>{
+    console.log(item);
+})
+
+//Mostre o texto dos paragrafos no console
+
+paragrafos.forEach((item)=>{
+    console.log(item.innerText);
+})
+
+//Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) =>{
+    console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach((item) => console.log(i++));
