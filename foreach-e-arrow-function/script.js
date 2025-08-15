4432b72722c83e99b4fde1653a9363585d3fcb8b
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-08-15 06:01:01 -0300

    aula , aprendendo sobre forEach e Arrow Function

---
+++ foreach-e-arrow-function/script.js
@@ -0,0 +1,23 @@
+/*const imgs = document.querySelectorAll('img');
+
+imgs.forEach(function(item, index, array){
+    //console.log(item, index, array);
+})
+*/
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function(item){
+    //console.log(item);
+})
+
+
+/*const imgs = document.querySelectorAll('img');
+imgs.forEach((item) => {
+    console.log(item);
+})
+*/
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item) =>console.log(item));
