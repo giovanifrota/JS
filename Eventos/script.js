4c2a2e891acc0ff094d78b2fed2617fac37d1a14
Giovani <giovanifrota45@gmail.com>
2025-08-19 14:39:52 -0300

    Aula de Eventos parte 1

---
+++ Eventos/script.js
@@ -0,0 +1,50 @@
+const img = document.querySelector('img');
+
+function callback(event){
+    console.log(event);
+}
+
+//img.addEventListener('click', callback);
+
+const imagensLista = document.querySelector('.animais-lista');
+
+function callbackLista(event){
+    console.log(event.currentTarget);
+    console.log(event.target);
+    console.log(event.type);
+}
+
+imagensLista.addEventListener('click', callbackLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno(event){
+    event.preventDefault();
+    //console.log(event);
+    console.log(this);
+}
+
+linkExterno.addEventListener('click', handleLinkExterno);
+
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event){
+    console.log(event.type, event);
+}
+
+//h1.addEventListener('mouseenter', handleEvent);
+//h1.addEventListener('mousemove', handleEvent);
+//h1.addEventListener('click', handleEvent);
+
+//window.addEventListener('scroll', handleEvent);
+//window.addEventListener('resize', handleEvent);
+
+function handleKeyboard(event){
+    if(event.key === 'f'){
+        document.body.classList.toggle('fullscren');
+    }
+    console.log(event.key);
+}
+
+window.addEventListener('keudown', handleKeyboard);
