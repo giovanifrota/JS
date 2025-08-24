fb680af8a64750dffe6e6e403a6ec686839ab27e
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-08-24 19:53:53 -0300

    Aprendendo na aula sobre Traversing e Manipulação

---
+++ transversing-e-manipulação/script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1');
+const animaisLista = document.querySelector('.animais-descricao');
+
+h1.innerHTML = '<p>Novo Titulo</p>'
+
+//console.log(animaisLista.innerHTML);
+
+const lista = document.querySelector('.animais-lista');
+
+console.log(lista.children);
