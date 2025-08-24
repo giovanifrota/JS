d8a36b78c15cc58c55a3c45d5997cab7f46f8f05
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-08-24 20:45:05 -0300

    Transversing e Manipulação 2 com exercicios

--- transversing-e-manipulação/script.js
+++ transversing-e-manipulação/script.js
@@ -1,10 +1,34 @@
+// const h1 = document.querySelector('h1');
+// const animaisLista = document.querySelector('.animais-descricao');
+
+// h1.innerHTML = '<p>Novo Titulo</p>'
+
+// //console.log(animaisLista.innerHTML);
+
+// const lista = document.querySelector('.animais-lista');
+
+// console.log(lista.children);
+
+// const animais = document.querySelector('.animais');
+// const contato = document.querySelector('.contato');
+// const Titulo = contato.querySelector('.titulo');
+
+// const mapa = document.querySelector('.mapa');
+
+// //contato.replaceChild(mapa, Titulo);
+
+// const novoh1 = document.createElement('h1');
+// novoh1.innerText = 'Novo titulo';
+// novoh1.classList.add('titulo');
+
+// mapa.appendChild(novoh1);
+
+// console.log(novoh1);
+
+
 const h1 = document.querySelector('h1');
-const animaisLista = document.querySelector('.animais-descricao');
+const faq = document.querySelector('.faq');
 
-h1.innerHTML = '<p>Novo Titulo</p>'
+const cloneh1 = h1.cloneNode(true);
 
-//console.log(animaisLista.innerHTML);
-
-const lista = document.querySelector('.animais-lista');
-
-console.log(lista.children);
+faq.appendChild(cloneh1);    
