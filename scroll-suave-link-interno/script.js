73351cf3b493c916f883bc426829d90ca4b8e661
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-08-26 17:29:41 -0300

    aprendendo scrool suave link interno

---
+++ scroll-suave-link-interno/script.js
@@ -0,0 +1,72 @@
+function initTabNav() {
+  const tabmenu = document.querySelectorAll(".js-tabmenu li");
+  const tabcontent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabcontent.length && tabmenu.length) {
+    tabcontent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabcontent[index].classList.add("ativo");
+    }
+
+    tabmenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
+
+function initScroolSuave() {
+  const linkInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    /*
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth'
+  })
+  */
+  }
+
+  linkInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScroolSuave();
