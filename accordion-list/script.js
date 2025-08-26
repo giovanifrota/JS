e1a8a24865f3103d81b7d49590f222e1dc23935d
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-08-26 17:03:54 -0300

    Accordion List

---
+++ accordion-list/script.js
@@ -0,0 +1,43 @@
+function initTabNav() {
+  const tabmenu = document.querySelectorAll(".js-tabmenu li");
+  const tabcontent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabcontent.length && tabmenu.length) {
+    tabcontent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabcontent[index].classList.add("ativo");
+    }
+
+    tabmenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
